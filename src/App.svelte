50ead4f7af9f727d3d986c23be23a05bf0d883a0
vjrj <vjrj@ourproject.org>
2020-09-07 01:07:39 +0200

    Added background

--- src/App.svelte
+++ src/App.svelte
@@ -134,139 +134,195 @@
 
 </script>
 
-<main style="--footer-height: {footerHeight};">
-  <SiteMenu conf={conf} save="{save}"/>
-  <Flex direction="column" align="stretch" justify="start">
-    <div class="main-flex">
-      <h2>Living Atlas Generator</h2>
-      {#if (page === 1)}
-        <IntroPage/>
-      {/if}
-      {#if (page === 2)}
-        <TextField bind:value={conf.LA_project_name} label="Your LA Project Long Name" error={longNameError}
-                   on:change={onChange} append={longNameAppend}/>
-        <TextField bind:value={conf.LA_project_shortname} label="Your LA Project Short Name" error={shortNameError}
-                   on:change={onChange} append={shortNameAppend}/>
-        <Tooltip>
-          <div slot="activator">
-            <Switch bind:value={conf.LA_enable_ssl} label="Use SSL?"></Switch>
-          </div>
-          Quite recommended
-        </Tooltip>
-        <Flex align="center" justify="start">
-          <UrlPrefix ssl={conf.LA_enable_ssl}/>
-          <TextField on:change={onChange} bind:value={conf.LA_domain} error={mainDomainError} append={mainDomainAppend}
-                     label="The domain of your LA node"/>
-        </Flex>
-      {/if}
+<main class="hero" style="--footer-height: {footerHeight};">
+	<SiteMenu conf={conf} save="{save}"/>
+	<Flex direction="column" align="stretch" justify="start">
+		<div class="main-flex">
+			<h2>Living Atlas Generator</h2>
+			{#if (page === 1)}
+				<IntroPage/>
+			{/if}
+			{#if (page === 2)}
+				<TextField bind:value={conf.LA_project_name} label="Your LA Project Long Name" error={longNameError}
+									 on:change={onChange} append={longNameAppend}/>
+				<TextField bind:value={conf.LA_project_shortname} label="Your LA Project Short Name" error={shortNameError}
+									 on:change={onChange} append={shortNameAppend}/>
+				<Tooltip>
+					<div slot="activator">
+						<Switch bind:value={conf.LA_enable_ssl} label="Use SSL?"></Switch>
+					</div>
+					Quite recommended
+				</Tooltip>
+				<Flex align="center" justify="start">
+					<UrlPrefix ssl={conf.LA_enable_ssl}/>
+					<TextField on:change={onChange} bind:value={conf.LA_domain} error={mainDomainError} append={mainDomainAppend}
+										 label="The domain of your LA node"/>
+				</Flex>
+			{/if}
 
-      {#if (page === 3)}
-        <TextField textarea rows=2 bind:value={conf.hostnames} append={hostnameAppend} error={hostnameError}
-                   on:change={onChange} outlined
-                   label="Names of the servers you will use (comma or space separated)"
-        />
-        <p>
-          <Card.Card>
-            <div slot="title">
-              <Card.Title title="Tips" d/>
-            </div>
-            <div slot="text" class="p-5 pb-5 pt-0 text-gray-700 body-2">
-              See the <a href="https://github.com/AtlasOfLivingAustralia/documentation/wiki/Infrastructure-Requirements"
-                         target="_blank"> infrastructure requirements page</a> and other portals infrastructure in
-              <a href="https://github.com/AtlasOfLivingAustralia/documentation/wiki/"
-                 target="_blank">our documentation wiki</a> to dimension your LA portal.
-              For a test portal a big server can host the main basic LA services.<br>
-              If you are unsure type something like "server1, server2, server3". You can modify this and the rest of
-              values later.
-            </div>
-          </Card.Card>
-        </p>
-      {/if}
+			{#if (page === 3)}
+				<TextField textarea rows=2 bind:value={conf.hostnames} append={hostnameAppend} error={hostnameError}
+									 on:change={onChange} outlined
+									 label="Names of the servers you will use (comma or space separated)"
+				/>
+				<p>
+					<Card.Card>
+						<div slot="title">
+							<Card.Title title="Tips" d/>
+						</div>
+						<div slot="text" class="p-5 pb-5 pt-0 text-gray-700 body-2">
+							See the <a href="https://github.com/AtlasOfLivingAustralia/documentation/wiki/Infrastructure-Requirements"
+												 target="_blank"> infrastructure requirements page</a> and other portals infrastructure in
+							<a href="https://github.com/AtlasOfLivingAustralia/documentation/wiki/"
+								 target="_blank">our documentation wiki</a> to dimension your LA portal.
+							For a test portal a big server can host the main basic LA services.<br>
+							If you are unsure type something like "server1, server2, server3". You can modify this and the rest of
+							values later.
+						</div>
+					</Card.Card>
+				</p>
+			{/if}
 
-      {#if (page === 4)}
-        <div class="to-left">
-          <h5 class="t-center">Define how your services URLs will look like:</h5>
-          <List class="" items={services}>
-            <li slot="item" let:item={item}>
-              <Service conf={conf} service={servicesStore[item]} save="{save}" debug
-                       hostnamesList={conf.hostnamesList}/>
-            </li>
-          </List>
-        </div>
-        <form target="_blank" method="post" action="/v1/gen" class="form-link">
-          <button id="submit-btn" type="submit" name="conf" value={JSON.stringify(conf)} class="link-button">
-          </button>
-        </form>
-      {/if}
-      <BtnArea {firstBtnDisabled} {onFirstBtnClick} {sndBtnDisabled} {onSndBtnClick} {sndBtnText} {footerHeight}/>
-    </div>
-  </Flex>
-  <Snackbar top bind:value={showSnackbarTop}>
-    <div>This is still in development. Come back soon!</div>
-  </Snackbar>
+			{#if (page === 4)}
+				<div class="to-left">
+					<h5 class="t-center">Define how your services URLs will look like:</h5>
+					<List class="" items={services}>
+						<li slot="item" let:item={item}>
+							<Service conf={conf} service={servicesStore[item]} save="{save}" debug
+											 hostnamesList={conf.hostnamesList}/>
+						</li>
+					</List>
+				</div>
+				<form target="_blank" method="post" action="/v1/gen" class="form-link">
+					<button id="submit-btn" type="submit" name="conf" value={JSON.stringify(conf)} class="link-button">
+					</button>
+				</form>
+			{/if}
+			<BtnArea {firstBtnDisabled} {onFirstBtnClick} {sndBtnDisabled} {onSndBtnClick} {sndBtnText} {footerHeight}/>
+		</div>
+	</Flex>
+	<Snackbar top bind:value={showSnackbarTop}>
+		<div>This is still in development. Come back soon!</div>
+	</Snackbar>
 </main>
 
 <style>
-  main {
-    /* text-align: center; */
-    /* padding: 30px; */
-    max-width: none;
-    margin: 0 auto;
-  }
-
-  h2, .t-center {
-    text-align: center;
-    padding-bottom: 20px;
-  }
-
-  .to-left, blockquote {
-    text-align: left;
-  }
-
-  @media (min-width: 640px) {
-    .main-flex {
-      padding: 30px;
-    }
-  }
-
-  @media (max-width: 640px) {
     main {
-      max-width: none;
+        /* text-align: center; */
+        /* padding: 30px; */
+        max-width: none;
+        margin: 0 auto;
+    }
+
+    h2, .t-center {
+        text-align: center;
+        padding-bottom: 20px;
+    }
+
+    .to-left, blockquote {
+        text-align: left;
+    }
+
+    @media (min-width: 640px) {
+        .main-flex {
+            padding: 30px;
+        }
+    }
+
+    @media (max-width: 640px) {
+        main {
+            max-width: none;
+        }
+
+        .main-flex {
+            padding: 10px;
+        }
+
+        h2 {
+            font-size: 32px;
+        }
+    }
+
+    @media (min-width: 640px) and (max-width: 1024px) {
+        main {
+            max-width: none;
+        }
+    }
+
+    @media (min-width: 1024px) and (max-width: 1920px) {
+        .main-flex {
+            margin: 0 100px;
+        }
+    }
+
+    @media (min-width: 1920px) {
+        .main-flex {
+            margin: 0 150px;
+        }
     }
 
     .main-flex {
-      padding: 10px;
+        padding-bottom: var(--footer-height);
     }
 
-    h2 {
-      font-size: 32px;
+    .form-link {
+        display: none;
     }
-  }
 
-  @media (min-width: 640px) and (max-width: 1024px) {
-    main {
-      max-width: none;
+    .hero::before {
+        background-size: cover;
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -2;
+        opacity: 0.1;
+        background-attachment: fixed;
     }
-  }
 
-  @media (min-width: 1024px) and (max-width: 1920px) {
-    .main-flex {
-      margin: 0 100px;
+    @media (min-width: 320px) {
+        .hero::before {
+            background-image: url(/images/back-460.jpg);
+        }
     }
-  }
 
-  @media (min-width: 1920px) {
-    .main-flex {
-      margin: 0 150px;
+    @media (min-width: 460px) {
+        .hero::before {
+            background-image: url(/images/back-720.jpg);
+        }
     }
-  }
 
-  .main-flex {
-    padding-bottom: var(--footer-height);
-  }
+    @media (min-width: 720px) {
+        .hero::before {
+            background-image: url(/images/back-980.jpg);
+        }
+    }
 
-  .form-link {
-    display: none;
-  }
+    @media (min-width: 980px) {
+        .hero::before {
+            background-image: url(/images/back-1240.jpg);
+        }
+    }
+
+    @media (min-width: 1240px) {
+        .hero::before {
+            background-image: url(/images/back-1500.jpg);
+        }
+    }
+
+    @media (min-width: 1500px) {
+        .hero::before {
+            background-image: url(/images/back-1760.jpg);
+        }
+    }
+
+    @media (min-width: 1760px) {
+        .hero::before {
+            background-image: url(/images/back-1920.jpg);
+        }
+    }
 
 </style>
